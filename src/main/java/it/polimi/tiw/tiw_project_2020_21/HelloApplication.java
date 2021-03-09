aac85207c8dfc8162c65acd3f2d6aca4f61b3bfa
Javinyx <javinbarone@gmail.com>
2021-03-09 12:43:45 +0100

    first commit

---
+++ src/main/java/it/polimi/tiw/tiw_project_2020_21/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.tiw_project_2020_21;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
