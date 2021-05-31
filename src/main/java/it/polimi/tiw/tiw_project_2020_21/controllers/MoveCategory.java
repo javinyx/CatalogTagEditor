42464a1fda0f7a1fc4ecf4d1423dfca376d16d3f
Verdax97 <Verdax97@users.noreply.github.com>
2021-05-31 13:59:43 +0200

    cose

---
+++ src/main/java/it/polimi/tiw/tiw_project_2020_21/controllers/MoveCategory.java
@@ -0,0 +1,47 @@
+package it.polimi.tiw.tiw_project_2020_21.controllers;
+
+import it.polimi.tiw.tiw_project_2020_21.beans.Category;
+import it.polimi.tiw.tiw_project_2020_21.dao.CategoryDAO;
+import it.polimi.tiw.tiw_project_2020_21.util.Initializer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet("/MoveCategory")
+public class MoveCategory extends HttpServlet
+{
+    private static Connection connection;
+    private static TemplateEngine templateEngine;
+
+    @Override
+    public void init() {
+        connection = Initializer.connectionInit(getServletContext());
+        templateEngine = Initializer.templateEngineInit(getServletContext());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.sendRedirect(getServletContext().getContextPath() + "/");
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            if(connection!=null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
